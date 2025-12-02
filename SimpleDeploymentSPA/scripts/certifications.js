c6f3b94cd0640d0091f314a81e16e506a5ba6b60
jon-tj <jh.tjemsland@gmail.com>
2025-12-02 17:09:27 +0100

    add certs

---
+++ SimpleDeploymentSPA/scripts/certifications.js
@@ -0,0 +1,37 @@
+const CERTIFICATIONS = [
+    {
+        title: "Microsoft Certified: Azure Developer Associate (AZ-204)",
+        issuer: "Microsoft",
+        issueDate: "2025-08",
+        credentialLink: "https://learn.microsoft.com/api/credentials/share/en-us/jontj-9073/616E65D8ECEFACD7?sharingId=3D8A207BA2DC3CAE",
+        description: [
+            `Validates skills in designing, building, testing, and maintaining cloud applications and services on Microsoft Azure. Demonstrates proficiency in developing Azure compute, storage, security, and integration solutions.`
+        ]
+    },
+    {
+        title: "Microsoft Certified: Azure DevOps Engineer Expert (AZ-400)",
+        issuer: "Microsoft",
+        issueDate: "2025-08",
+        credentialLink: "https://learn.microsoft.com/api/credentials/share/en-us/jontj-9073/B0F03EB5F06CE4FD?sharingId=3D8A207BA2DC3CAE",
+        description: [
+            `Validates expertise in combining people, processes, and technologies to continuously deliver valuable products and services that meet end-user needs and business objectives on Azure.`
+        ]
+    },
+    {
+        title: "GitHub Foundations",
+        issuer: "GitHub",
+        issueDate: "2025-10",
+        description: [
+            `Recognizes foundational knowledge of GitHub, including version control, collaboration, and workflow best practices for software development projects.`
+        ]
+    },
+    {
+        title: "Certified Scrum Master (CSM)",
+        issuer: "Scrum Alliance",
+        issueDate: "2025-11",
+        credentialLink: "https://www.scrum.org/certificates/1264866",
+        description: [
+            `Demonstrates understanding of Scrum framework principles and practices, including team roles, events, and artifacts. Validates ability to facilitate Agile teams and foster collaboration and continuous improvement.`
+        ]
+    }
+];

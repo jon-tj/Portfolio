9bc4314941b9476329e60025e886facec7766e45
jon-tj <jh.tjemsland@gmail.com>
2025-12-03 11:47:47 +0100

    add work experience

---
+++ SimpleDeploymentSPA/scripts/work-experience.js
@@ -0,0 +1,59 @@
+const WORK_EXPERIENCES = [
+    {
+        employer: "Sopra Steria",
+        role: "Software Engineer",
+        duration: ["2025-08", "present"],
+        paragraphs: [
+            `Working as a full-stack developer and consultant. Assigned to Halliburton, contributing to the development of advanced digital solutions and gaining experience in large-scale, industry-focused software delivery.`
+        ],
+        tags: [
+            "python",
+            "c#",
+            "azure",
+            "devops",
+            "gitlab",
+            "js",
+            "java"
+        ]
+    },
+    {
+        employer: "University of Stavanger",
+        role: "Teaching Assistant",
+        duration: ["2023-08", "2025-05"],
+        paragraphs: [
+            `Assisted students with coursework, exercises, and concepts across subjects such as web development, algorithms, and data structures. Helped students understand challenging topics and guided them through problem-solving.`,
+            `Courses I helped teach: Advanced programming (DAT240), Algorithms and data structures (DAT200), Web programming (DAT310), Applied mathematics and programming robots (ELE130), Python programming (DAT120)`
+        ],
+        tags: [
+            "c#",
+            "python",
+            "html",
+            "sqlite",
+            "git"
+        ]
+    },
+    {
+        employer: "Bouvet",
+        role: "Software Developer",
+        duration: ["2025-06", "2025-08"],
+        paragraphs: [
+            `Worked with DNV Imatis to further develop a hospital system used for ordering patient transport. Contributed to backend features in .NET and frontend development using JavaScript and HTML, with GitHub for collaboration and version control.`
+        ],
+        tags: [
+            "c#",
+            "core",
+            "python",
+            "html",
+            "git",
+        ]
+    },
+    {
+        employer: "Norwegian Armed Forces",
+        role: "Private",
+        duration: ["2021-08", "2022-07"],
+        paragraphs: [
+            `Performed guard duties and contributed to daily operational readiness. Developed discipline, teamwork, and reliability under demanding conditions.`
+        ],
+        tags: []
+    }
+]
